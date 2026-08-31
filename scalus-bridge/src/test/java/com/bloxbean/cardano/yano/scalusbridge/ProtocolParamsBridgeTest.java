2d02346e82c8bc02fae809cb67425fa7d702e259
Satya <satran004@gmail.com>
2026-08-31 19:37:45 +0800

    test(scalus): guard protocol parameter mapping

--- scalus-bridge/src/test/java/com/bloxbean/cardano/yano/scalusbridge/ProtocolParamsBridgeTest.java
+++ scalus-bridge/src/test/java/com/bloxbean/cardano/yano/scalusbridge/ProtocolParamsBridgeTest.java
@@ -6,6 +6,7 @@ import scalus.cardano.ledger.NonNegativeInterval;
 import scalus.cardano.ledger.UnitInterval;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.LinkedHashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,6 +14,121 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ProtocolParamsBridgeTest {
+
+    @Test
+    void everyProtocolParameterMapsToItsOwnField() {
+        ProtocolParams pp = baseParams(11);
+        pp.setProtocolMinorVer(1);
+        pp.setMinFeeA(45);
+        pp.setMinFeeB(155382);
+        pp.setMaxBlockSize(90113);
+        pp.setMaxTxSize(16385);
+        pp.setMaxBlockHeaderSize(1101);
+        pp.setKeyDeposit("2000001");
+        pp.setPoolDeposit("500000009");
+        pp.setEMax(19);
+        pp.setNOpt(501);
+        pp.setA0(new BigDecimal("0.31"));
+        pp.setRho(new BigDecimal("0.004"));
+        pp.setTau(new BigDecimal("0.22"));
+        pp.setMinPoolCost("340000001");
+
+        var plutusV1 = new LinkedHashMap<String, Long>();
+        plutusV1.put("000", 61L);
+        plutusV1.put("001", 62L);
+        var costModels = new LinkedHashMap<String, LinkedHashMap<String, Long>>();
+        costModels.put("PlutusV1", plutusV1);
+        pp.setCostModels(costModels);
+        pp.setPriceMem(new BigDecimal("0.0577"));
+        pp.setPriceStep(new BigDecimal("0.0000721"));
+        pp.setMaxTxExMem("10000001");
+        pp.setMaxTxExSteps("10000000001");
+        pp.setMaxBlockExMem("50000001");
+        pp.setMaxBlockExSteps("40000000001");
+        pp.setMaxValSize("5001");
+        pp.setCollateralPercent(new BigDecimal("152"));
+        pp.setMaxCollateralInputs(4);
+        pp.setCoinsPerUtxoSize("4311");
+
+        pp.setGovActionDeposit(BigInteger.valueOf(100_000_000_003L));
+        pp.setGovActionLifetime(7);
+        pp.setDrepDeposit(BigInteger.valueOf(500_000_003L));
+        pp.setDrepActivity(21);
+        pp.setCommitteeMinSize(5);
+        pp.setCommitteeMaxTermLength(148);
+        pp.setMinFeeRefScriptCostPerByte(new BigDecimal("16"));
+        pp.setPvtMotionNoConfidence(new BigDecimal("0.41"));
+        pp.setPvtCommitteeNormal(new BigDecimal("0.42"));
+        pp.setPvtCommitteeNoConfidence(new BigDecimal("0.43"));
+        pp.setPvtHardForkInitiation(new BigDecimal("0.44"));
+        pp.setPvtPPSecurityGroup(new BigDecimal("0.45"));
+        pp.setDvtMotionNoConfidence(new BigDecimal("0.51"));
+        pp.setDvtCommitteeNormal(new BigDecimal("0.52"));
+        pp.setDvtCommitteeNoConfidence(new BigDecimal("0.53"));
+        pp.setDvtUpdateToConstitution(new BigDecimal("0.54"));
+        pp.setDvtHardForkInitiation(new BigDecimal("0.55"));
+        pp.setDvtPPNetworkGroup(new BigDecimal("0.56"));
+        pp.setDvtPPEconomicGroup(new BigDecimal("0.57"));
+        pp.setDvtPPTechnicalGroup(new BigDecimal("0.58"));
+        pp.setDvtPPGovGroup(new BigDecimal("0.59"));
+        pp.setDvtTreasuryWithdrawal(new BigDecimal("0.60"));
+
+        var scalusParams = ProtocolParamsBridge$.MODULE$.toScalusProtocolParams(pp);
+
+        assertEquals(152L, scalusParams.collateralPercentage());
+        assertEquals(148L, scalusParams.committeeMaxTermLength());
+        assertEquals(5L, scalusParams.committeeMinSize());
+        assertEquals(1, scalusParams.costModels().models().size());
+        var convertedCostModel = scalusParams.costModels().models().values().head();
+        assertEquals(2, convertedCostModel.size());
+        assertEquals(61L, ((Number) convertedCostModel.apply(0)).longValue());
+        assertEquals(62L, ((Number) convertedCostModel.apply(1)).longValue());
+        assertEquals(21L, scalusParams.dRepActivity());
+        assertEquals(500_000_003L, scalusParams.dRepDeposit());
+        assertUnitInterval(scalusParams.dRepVotingThresholds().motionNoConfidence(), 51, 100);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().committeeNormal(), 13, 25);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().committeeNoConfidence(), 53, 100);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().updateToConstitution(), 27, 50);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().hardForkInitiation(), 11, 20);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().ppNetworkGroup(), 14, 25);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().ppEconomicGroup(), 57, 100);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().ppTechnicalGroup(), 29, 50);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().ppGovGroup(), 59, 100);
+        assertUnitInterval(scalusParams.dRepVotingThresholds().treasuryWithdrawal(), 3, 5);
+        assertNonNegativeInterval(scalusParams.executionUnitPrices().priceMemory(), 577, 10_000);
+        assertNonNegativeInterval(scalusParams.executionUnitPrices().priceSteps(), 721, 10_000_000);
+        assertEquals(100_000_000_003L, scalusParams.govActionDeposit());
+        assertEquals(7L, scalusParams.govActionLifetime());
+        assertEquals(90_113L, scalusParams.maxBlockBodySize());
+        assertEquals(50_000_001L, scalusParams.maxBlockExecutionUnits().memory());
+        assertEquals(40_000_000_001L, scalusParams.maxBlockExecutionUnits().steps());
+        assertEquals(1_101L, scalusParams.maxBlockHeaderSize());
+        assertEquals(4L, scalusParams.maxCollateralInputs());
+        assertEquals(10_000_001L, scalusParams.maxTxExecutionUnits().memory());
+        assertEquals(10_000_000_001L, scalusParams.maxTxExecutionUnits().steps());
+        assertEquals(16_385L, scalusParams.maxTxSize());
+        assertEquals(5_001L, scalusParams.maxValueSize());
+        assertEquals(16L, scalusParams.minFeeRefScriptCostPerByte());
+        assertEquals(340_000_001L, scalusParams.minPoolCost());
+        assertEquals(0.004, scalusParams.monetaryExpansion());
+        assertEquals(0.31, scalusParams.poolPledgeInfluence());
+        assertEquals(19L, scalusParams.poolRetireMaxEpoch());
+        assertUnitInterval(scalusParams.poolVotingThresholds().motionNoConfidence(), 41, 100);
+        assertUnitInterval(scalusParams.poolVotingThresholds().committeeNormal(), 21, 50);
+        assertUnitInterval(scalusParams.poolVotingThresholds().committeeNoConfidence(), 43, 100);
+        assertUnitInterval(scalusParams.poolVotingThresholds().hardForkInitiation(), 11, 25);
+        assertUnitInterval(scalusParams.poolVotingThresholds().ppSecurityGroup(), 9, 20);
+        assertEquals(11, scalusParams.protocolVersion().major());
+        assertEquals(1, scalusParams.protocolVersion().minor());
+        assertEquals(2_000_001L, scalusParams.stakeAddressDeposit());
+        assertEquals(500_000_009L, scalusParams.stakePoolDeposit());
+        assertEquals(501L, scalusParams.stakePoolTargetNum());
+        assertEquals(0.22, scalusParams.treasuryCut());
+        // The A/B crossing is intentional: minFeeB is fixed and minFeeA is per byte.
+        assertEquals(155_382L, scalusParams.txFeeFixed());
+        assertEquals(45L, scalusParams.txFeePerByte());
+        assertEquals(4_311L, scalusParams.utxoCostPerByte());
+    }
 
     @Test
     void executionUnitLimitsKeepMemoryAndStepsInScalusOrder() {
@@ -179,5 +295,15 @@ class ProtocolParamsBridgeTest {
         var costModels = new LinkedHashMap<String, LinkedHashMap<String, Long>>();
         costModels.put("PlutusV1", plutusV1);
         return costModels;
+    }
+
+    private void assertUnitInterval(UnitInterval actual, long numerator, long denominator) {
+        assertEquals(numerator, actual.numerator());
+        assertEquals(denominator, actual.denominator());
+    }
+
+    private void assertNonNegativeInterval(NonNegativeInterval actual, long numerator, long denominator) {
+        assertEquals(numerator, actual.numerator());
+        assertEquals(denominator, actual.denominator());
     }
 }
